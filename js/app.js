89d02ebfd13a3e8c64b9e86ae1531569ca183695
JalalHasan-22 <jhasanj8@gmail.com>
2022-01-23 14:38:04 +0200

    Added three yes/no questions; commented previous code to have less prompts

--- js/app.js
+++ js/app.js
@@ -1,20 +1,55 @@
 "use strict";
 
-const userName = prompt("Please enter your name")?.toLocaleLowerCase().trim();
-const gender = prompt("What is your Gender?")?.toLocaleLowerCase().trim();
-const age = +prompt("How old are you ?");
-let welcomingMessage = "";
-let title = "";
-let displayWelcomeMessage;
+// const userName = prompt("Please enter your name")?.toLocaleLowerCase().trim();
+// const gender = prompt("What is your Gender?")?.toLocaleLowerCase().trim();
+// const age = +prompt("How old are you ?");
+// let welcomingMessage = "";
+// let title = "";
+// let displayWelcomeMessage;
 
-if (age <= 0) alert("Age cannot be less than or equal to zero");
+let answersArr = [];
+let firstQuestionAnswer = prompt(
+  "1) Are you commited to finish all of your tasks ? Yes / No"
+)
+  ?.toLocaleUpperCase()
+  .charAt(0);
+let secondQuestionAnswer = prompt(
+  "2) is it essential to finish all your tasks every day ? Yes / No"
+)
+  ?.toLocaleUpperCase()
+  .charAt(0);
+let thirdQuestionAnswer = prompt("3) Are you an organized person ? Yes / No")
+  ?.toLocaleUpperCase()
+  .charAt(0);
 
-// If user did not enter a name, No need for the welcoming message
-if (userName) {
-  displayWelcomeMessage = confirm(
-    "Would you like to display the welcome message?"
-  );
-}
+const answerValidation = function (answer) {
+  if (answer === "Y") answer = "Yes";
+  else if (answer === "N") answer = "No";
+  else answer = "Invalid";
+
+  answersArr.push(answer);
+};
+
+answerValidation(firstQuestionAnswer);
+answerValidation(secondQuestionAnswer);
+answerValidation(thirdQuestionAnswer);
+
+const printAnswers = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+printAnswers(answersArr);
+// if (age <= 0) alert("Age cannot be less than or equal to zero");
+
+// // If user did not enter a name, No need for the welcoming message
+// if (userName) {
+//   displayWelcomeMessage = confirm(
+//     "Would you like to display the welcome message?"
+//   );
+// }
+
 // switch (gender) {
 //   case "male":
 //     title = "Mr";
@@ -29,11 +64,12 @@ if (userName) {
 //     title = "Ms";
 //     break;
 // }
-if (gender === "male" || gender === "m") title = "Mr";
-if (gender === "female" || gender === "f") title = "Ms";
 
-if (displayWelcomeMessage) {
-  welcomingMessage = `Good Day ${title ? title : ""} ${userName}`;
-  alert(welcomingMessage);
-} else {
-}
+// if (gender === "male" || gender === "m") title = "Mr";
+// if (gender === "female" || gender === "f") title = "Ms";
+
+// if (displayWelcomeMessage) {
+//   welcomingMessage = `Good Day ${title ? title : ""} ${userName}`;
+//   alert(welcomingMessage);
+// } else {
+// }
